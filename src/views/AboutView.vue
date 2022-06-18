e58d87b37a41e4b3d3e7fd7a73f5686b0af69835
pratamafarhan10 <pratamafarhan10@gmail.com>
2022-06-18 08:02:27 +0700

    kosongan

--- src/views/AboutView.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="about">
-    <h1 class="text-red-500">This is an about page</h1>
-    <span class="bg-red-500">
-      <font-awesome-icon icon="Ambulance" />
-    </span>
-  </div>
-</template>
